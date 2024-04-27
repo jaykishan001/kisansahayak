b97f0ea490cd70a4b9e90f2714fe03ec2528990d
jaykishan001 <davidjsd90@gmail.com>
2024-04-28 01:07:52 +0530

    post card

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import service from '../appwrite/config'
 
 function PostCard({$id , product, expirty, bought, featuredImage, cartegory, quantity, }) {
   return (
-    <div>PostCard</div>
+    <div>
+        <div className="flex justify-between">
+            <div className="flex">
+            <img src={service.getFilePreview(featuredImage)} alt="product" className="w-20 h-20" />
+            <div className="ml-4">
+                <h2>{product}</h2>
+                <p>{cartegory}</p>
+                <p>{quantity}</p>
+            </div>
+            </div>
+            <div>
+            <p>Expiry: {expirty}</p>
+            <p>Bought: {bought}</p>
+            </div>
+        </div>
+    </div>
   )
 }
 
