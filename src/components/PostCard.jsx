0cf4292c37b2609349ea1317c79b6c088b5de0e0
jaykishan001 <davidjsd90@gmail.com>
2024-04-28 01:04:20 +0530

    post

---
+++ src/components/PostCard.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function PostCard({$id , product, expirty, bought, featuredImage, cartegory, quantity, }) {
+  return (
+    <div>PostCard</div>
+  )
+}
+
+export default PostCard
